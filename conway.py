8b950668eb1d0d9d14f8d2ea8b0545b45f47b8b2
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-11 12:31:40 -0400

    Cells can live and die in the emoji Gridiverse.
    * Basic outline for Cell and Grid
    * Wrote hella wicked tests

--- conway.py
+++ conway.py
@@ -3,5 +3,42 @@
 # Dead with == 3 : live
 # Live with > 3 : die
 
-def main():
-    pass
+
+class Cell:
+    alive = False
+
+    def is_alive(self):
+        return self.alive
+    
+    def is_dead(self):
+        return not self.alive
+
+    def enliven(self):
+        self.alive = True
+
+    def kill(self):
+        self.alive = False
+
+    def __repr__(self):
+        if self.is_alive():
+            return "🥞"
+        else:
+            return "💀"
+
+    def __str__(self):
+        return "🥞"
+
+
+class Gridiverse:
+    """
+    This holds the cells, gently, lovingly.
+    """
+    grid = []
+
+    def __init__(self, width):
+        self.size = width * width
+        self.grid = [Cell() for num in range(self.size)]
+
+    def dead_cell_count(self):
+        deadies = [cell for cell in self.grid if cell.is_dead()]
+        return len(deadies)
--- tests/test_conway.py
+++ tests/test_conway.py
@@ -1,4 +1,31 @@
 from .context import conway
+from conway import (Cell, Gridiverse)
 
 def test_main():
     assert True
+
+def test_cell_can_be_created():
+    assert Cell()
+
+def test_cell_starts_dead():
+    cell = Cell()
+    assert cell.is_dead()
+
+def test_enliven():
+    cell = Cell()
+    cell.enliven()
+    assert cell.is_alive()
+
+def test_kill():
+    cell = Cell()
+    cell.kill()
+    assert cell.is_dead()
+
+def test_gridiverse():
+    width = 20
+    grid = Gridiverse(width)
+    assert grid.size == width * width
+
+def test_gridiverse_has_cells():
+    grid = Gridiverse(10)
+    assert grid.dead_cell_count() == 100
