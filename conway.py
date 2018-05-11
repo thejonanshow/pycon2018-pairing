2fcfe965b5186201e233a212cf314fb4f8998ec7
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-11 17:21:29 -0400

    WE ARE PYTHON EXPERTS

--- conway.py
+++ conway.py
@@ -5,3 +5,60 @@
 
 def main():
     pass
+
+
+class Cell():
+    def __init__(self):
+        self.live = False
+
+    def is_alive(self):
+        return self.live == True
+
+    def is_dead(self):
+        return self.live == False
+
+    def murder(self):
+        self.live = False
+
+    def resurrect(self):
+        self.live = True
+
+    def __iter__(self):
+        yield "turtle"
+
+    def next(self):
+        pass
+
+    def __str__(self):
+        return "💀" if self.is_dead() else "😇"
+
+    def __repr__(self):
+        return "🤖" if self.is_dead() else "👻"
+
+
+class Board():
+    def __init__(self):
+        self.width = 25
+        self.cells = [[Cell() for i in range(self.width)] for j in range(self.width)]
+
+    def get_width(self, mykeyword):
+        return self.width
+
+    def get_cell(self, xpos = 0, ypos = 0):
+        return self.cells[xpos][ypos] 
+
+    def __str__(self):
+        result = ""
+
+        for row in self.cells:
+            for cell in row:
+                result = result + str(cell)
+
+            result = result + "\n"
+
+        return result
+
+    def __repr__(self):
+        return "<Board - width: {width}>".format(width=self.width, height=2)
+
+print(Board())
--- tests/test_conway.py
+++ tests/test_conway.py
@@ -1,4 +1,38 @@
 from .context import conway
+from conway import Cell, Board
 
 def test_main():
     assert True
+
+def test_creating_cell():
+    assert Cell()
+
+def test_new_cells_are_dead():
+    assert Cell().is_dead()
+
+def test_cells_can_live():
+    test_cell = Cell()
+    test_cell.resurrect()
+    assert test_cell.is_alive()
+
+def test_murder():
+    test_cell = Cell()
+    test_cell.murder()
+    assert test_cell.is_dead()
+
+def test_board_is_twenty_five_bigness():
+    test_board = Board()
+    assert test_board.get_width() == 25
+
+def test_get_cell():
+    test_board = Board()
+    assert type(test_board.get_cell()) == Cell
+
+def test_that_cells_contain_turtles():
+    test_cell = Cell()
+    assert "turtle" in test_cell
+
+def test_draw_board():
+    test_board = Board()
+    assert "💀" in str(test_board)
+
