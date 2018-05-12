9f543d351054ef756d41ee23d0b428afaaa1fba7
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-12 11:23:08 -0400

    Thank you Luckner!

--- conway.py
+++ conway.py
@@ -1,7 +1,54 @@
 # Live with < 2 : die
-# Live with 2 or 3 : live
-# Dead with == 3 : live
+# Live with 2 or 3 : wake
+# Dead with == 3 : wake
 # Live with > 3 : die
 
-def main():
-    pass
+from random import randint
+
+
+class Square():
+    def __init__(self):
+        self.awake = True
+
+    @property
+    def is_awake(self):
+        return self.awake
+
+    @property
+    def background(self):
+        if self.is_awake:
+            return 'grey'
+        else:
+            return 'black'
+
+    def sleepify(self):
+        self.awake = False
+
+    def __str__(self):
+        if self.is_awake:
+            return "[O]"
+        else:
+            return "[X]"
+
+class Board():
+    def __init__(self, width = 25):
+        self.spaces = [[Square() for i in range(width)] for j in range(width)]
+
+        for row in self.spaces:
+            for square in row:
+                if randint(1, 2) == 2:
+                    square.sleepify()
+
+    def __str__(self):
+        output = ""
+        for row in self.spaces:
+            for square in row:
+                output = output + str(square)
+
+            output = output + "\n"
+
+        return output
+
+
+board = Board()
+print(board)
--- tests/test_conway.py
+++ tests/test_conway.py
@@ -1,4 +1,16 @@
 from .context import conway
+from conway import Square
 
 def test_main():
     assert True
+
+def test_square__initial_state():
+    assert Square().is_awake
+
+def test_square__background__awake_state():
+    assert Square().background == 'grey'
+
+def test_square__background__sleep_state():
+    square = Square()
+    square.sleepify()
+    assert square.background == 'black'
