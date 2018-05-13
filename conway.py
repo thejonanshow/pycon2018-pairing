94b9501992fd9ab5c3f7a9ff5b3a6db76eaee799
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-13 14:04:05 -0400

    Thank you Mo!

--- conway.py
+++ conway.py
@@ -2,6 +2,17 @@
 # Live with 2 or 3 : live
 # Dead with == 3 : live
 # Live with > 3 : die
+WIDTH_DEFAULT=25
+HEIGHT_DEFAULT=25
+DEAD = False
+LIVE = True
 
-def main():
-    pass
+"tRuE" == "true".casefold()
+
+class Board:
+    
+    def __init__(self,width=WIDTH_DEFAULT,height=HEIGHT_DEFAULT):
+        self.width= lambda: width
+        self.height= lambda: height
+
+        self._board=  
--- tests/test_conway.py
+++ tests/test_conway.py
@@ -1,4 +1,22 @@
 from .context import conway
+from conway import Board
 
-def test_main():
-    assert True
+def test_default_board_dimensions():
+    test_board = Board()
+    assert test_board.width() == 25
+    assert test_board.height() == 25
+
+def test_board_has_given_dimensions():
+    test_board = Board(width=30, height=30)
+    assert test_board.width() == 30
+    assert test_board.height() == 30
+
+def test_board_internal_structure_created():
+    test_board = Board()
+    test_board._board
+
+def test_board_internal_structure_dimensions():
+    test_board = Board(width=35, height=40)
+    assert len(test_board._board) == 35
+    assert len(test_board._board[0]) == 40
+
