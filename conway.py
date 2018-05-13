8e3fa2e1a4124ffc11ef55b17f47a852eff68d0c
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-13 13:02:14 -0400

    Thank you Max!!!

--- conway.py
+++ conway.py
@@ -2,6 +2,41 @@
 # Live with 2 or 3 : live
 # Dead with == 3 : live
 # Live with > 3 : die
+import random
 
-def main():
-    pass
+
+class Matrix:
+
+    def __init__(self, width=50, height=25):
+        self.width = width
+        self.height = height
+        self.grid = []
+        for column in range(self.height * self.width):
+            self.grid.append(Cell())
+
+    def __str__(self):
+        for cell in self.grid:
+            print(cell)
+
+class Cell:
+
+    def __init__(self):
+        dead_chance = random.randint(1, 100)
+        if dead_chance > 50:
+            self.dead = True
+        else:
+            self.dead = False
+    def die(self):
+        self.dead = True
+        # if you wanna do other things here
+
+    def live(self):
+        self.dead = False
+
+    def __str__(self):
+        if self.dead:
+            return "💀"
+        else:
+            return "😀"
+
+print(Matrix())
--- tests/test_conway.py
+++ tests/test_conway.py
@@ -1,4 +1,24 @@
 from .context import conway
+from conway import Matrix
+from conway import Cell
 
-def test_main():
-    assert True
+def test_matrix_can_be_created():
+    assert Matrix()
+
+def test_matrix_size():
+    assert Matrix().width == 50
+    assert Matrix().height == 25
+
+def test_matrix_starts_with_cells():
+    assert type(Matrix().grid[0]) == Cell
+
+def test_cell_can_be_dead():
+    assert Matrix().grid[0].dead
+
+def test_cell_can_be_killed():
+    test_cell = Cell()
+    test_cell.live()
+    assert not test_cell.dead
+
+    test_cell.die()
+    assert test_cell.dead
