e41678ea1b204c659a61185e439ec02645375663
Jonan Scheffler <jonanscheffler@gmail.com>
2018-05-13 12:01:40 -0400

    Thank you Leah!

--- conway.py
+++ conway.py
@@ -3,5 +3,15 @@
 # Dead with == 3 : live
 # Live with > 3 : die
 
-def main():
-    pass
+
+class Grid():
+    def __init__(self, width=50, height=25):
+        self.layout = [[None for i in range(height)] for j in range(width)]
+
+    @property
+    def width(self):
+        len(self.layout)
+
+    @property
+    def height(self):
+        len(self.layout[0])
--- tests/test_conway.py
+++ tests/test_conway.py
@@ -1,4 +1,7 @@
 from .context import conway
+from conway import Grid
 
-def test_main():
-    assert True
+def test_grid_has_size():
+    grid = Grid()
+    assert grid.width == 50
+    assert grid.height == 25
